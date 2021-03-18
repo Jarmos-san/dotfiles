a5cfd181c0732ea03fbb40a6b24dc49915ea2fe6
Jarmos-san <somraj.mle@gmail.com>
2021-03-18 17:25:22 +0530

    :fire: Add the installation file

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -1,6 +1,7 @@
 # Script to install necessary software
 
-# TODO: Installation script for Starship Cross-Shell Prompt 
-#       More info are available at:
-#           https://docs.microsoft.com/en-us/powershell/module/Microsoft.PowerShell.Archive/Expand-Archive?view=powershell-7.2
-#           https://stackoverflow.com/questions/41895772/powershell-script-to-download-a-zip-file-and-unzip-it
+# TODO: Installation script for Starship Cross-Shell Prompt
+#   More info are available at:
+#       https://docs.microsoft.com/en-us/powershell/module/Microsoft.PowerShell.Archive/Expand-Archive?view=powershell-7.2
+#       https://stackoverflow.com/questions/41895772/powershell-script-to-download-a-zip-file-and-unzip-it
+
