488d439af31eb6ebccd159ee86c23c4f9d98f358
Jarmos-san <somraj.mle@gmail.com>
2021-04-26 17:53:23 +0530

    :truck: Rename software installation script according to best practices

---
+++ Windows/Install-Software.ps1
@@ -0,0 +1,48 @@
+# Script to install necessary software
+
+# TODO: Installation script for Starship Cross-Shell Prompt
+#   More info are available at:
+#       https://docs.microsoft.com/en-us/powershell/module/Microsoft.PowerShell.Archive/Expand-Archive?view=powershell-7.2
+#       https://stackoverflow.com/questions/41895772/powershell-script-to-download-a-zip-file-and-unzip-it
+# TODO: Refactor the code. Write a function to parse a JSON file with all the required information instead.
+# References: https://gist.github.com/gavincampbell/05c803654ff70d21d538b49f0e363a6a
+#             https://gist.github.com/MarkTiedemann/c0adc1701f3f5c215fc2c2d5b1d5efd3
+Write-Host "Installing Vim"
+winget install -e --id vim.vim
+Write-Host "Vim installed!"
+
+Write-Host "Installing PowerShell Core"
+winget install -e --id Microsoft.PowerShell
+Write-Host "PowerShell Core installed!"
+
+Write-Host "Installing Steam"
+winget install -e --id Valve.Steam
+Write-Host "Steam installed!"
+
+Write-Host "Installing Visual Studio Code"
+winget install -e --id Microsoft.VisualStudio.Code
+Write-Host "Visual Studio Code installed!"
+
+Write-Host "Installing Alacritty"
+winget install -e --id Alacritty.Alacritty
+Write-Host "Alacritty installed!"
+
+Write-Host "Installing Transmission Bittorrent Client"
+winget install -e --id Transmission.Transmission
+Write-Host "Transmission Bittorent Client installed!"
+
+Write-Host "Installing VLC Media Player"
+winget install -e --id VideoLAN.VLC
+Write-Host "VLC Media Player installed"
+
+Write-Host "Installing Python"
+winget install -e --id Python.Python
+Write-Host "Python installed"
+
+Write-Host "Installing ShareX"
+winget install -e --id ShareX.ShareX
+Write-Host "ShareX installed"
+
+Write-Host "Installing Google Backup & Sync"
+winget install -e --id Google.BackupAndSync
+Write-Host " Google Backup & Sync installed"
