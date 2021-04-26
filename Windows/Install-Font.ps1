dee3b3e6fba7218400858d78582841d61c8aef9c
Jarmos-san <somraj.mle@gmail.com>
2021-04-26 17:11:12 +0530

    :poop: Update font installation script to download & extract zip file

--- Windows/Install-Font.ps1
+++ Windows/Install-Font.ps1
@@ -23,4 +23,27 @@ For now it's URI is hardcoded but it might be advisable to dynamically download 
 * -> Copy the fonts into "C:\Windows\Fonts"
 * -> Make a Registry entry at "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
 * -> Then delete the extracted files as well as the zipped file.
+
+TODO:
+    ? Copy only the relevant fonts to the C:\Windows\Fonts folder as well as the Registry.
+    ? Add Progress Bar while downloading the zipped file.
+    ? Use variables & Environment variables instead of hard coding. For e.g the $output variable.
+    ? Refactor code into proper functions & what not. It should confer to good coding practices & best practices.
 #>
+
+# Downloads the zipped file
+$url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
+$output = "C:\Users\Space\Downloads\FiraCode.zip"
+
+# Downloads the zipped file
+Write-Host "Downloading the zipped file..."
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+Write-Host "Done..."
+
+# Extract & then deletes the zipped file
+Write-Host "Extracting the zipped file..."
+Expand-Archive -Path $output -DestinationPath "C:\Users\Space\Downloads\FiraCode" -Force
+Write-Host "Extraction complete..."
+Write-Host "Deleting the redundant zipped file..."
+Remove-Item $output
+Write-Host "Redundant zipped file removed..."
