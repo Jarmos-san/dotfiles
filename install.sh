2756681c360cd126c39856397b6070b47be3c558
Somraj Saha <somraj.1994@gmail.com>
2021-10-06 12:56:26 +0530

    :wrench: Add minor TODO comment

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ echo "Hello, World!"
 # 1. Check if Homebrew exists, if not install it
 # 2. Check if Git exists, if not install it using Homebrew
 # 3. Once Git is available, clone the repo to ~
-# 4. Manage the config files by symlinking using GNU Stow
+# 4. Install all necessary software
+# 5. Manage the config files by symlinking using GNU Stow
 
 # For reference check the following repositories:
 # - https://github.com/mathiasbynens/dotfiles
