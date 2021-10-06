d978428541582e45b5728ca8b2fd90fcfea71198
Somraj Saha <somraj.1994@gmail.com>
2021-10-06 12:51:42 +0530

    :wrench: Add TODO comments to the install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Hello, World!"
+
+# TODO: Write a complete installation script
+
+# Installation & configuration steps
+# 1. Check if Homebrew exists, if not install it
+# 2. Check if Git exists, if not install it using Homebrew
+# 3. Once Git is available, clone the repo to ~
+# 4. Manage the config files by symlinking using GNU Stow
+
+# For reference check the following repositories:
+# - https://github.com/mathiasbynens/dotfiles
+# - https://github.com/disrupted/dotfiles
