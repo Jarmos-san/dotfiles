fc713489821953d9de2019aa40c0b42f4664ebfd
Somraj Saha <somraj.1994@gmail.com>
2021-10-07 13:55:41 +0530

    :pencil2: Add TODO comments for future reference

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ echo "Hello, World!"
 # 3. Once Git is available, clone the repo to ~
 # 4. Install all necessary software
 # 5. Manage the config files by symlinking using GNU Stow
+# 6. Create necessary folders like "projects", "work" & such
+# 7. Setup SSH, GPG & WSL config files (at /etc/wsl.conf)
 
 # For reference check the following repositories:
 # - https://github.com/mathiasbynens/dotfiles
