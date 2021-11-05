74b12328642d25a01753d72d78a9180694fbdcce
Somraj Saha <somraj.mle@gmail.com>
2021-11-05 16:19:44 +0530

    :construction_worker: Update bootstrap file to download personal projects as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,14 @@ curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | 
 
 # TODO: Check if "git" exists, if not install using Homebrew
 
+echo "Creating necessary folders"
+mkdir -p "projects" "work"
+
+echo "Downloading personal projects"
+cd projects
+git clone git@github.com:Jarmos-san/blog
+git clone git@github.com:Jarmos-san/jarvim
+
 echo "Downloading dotfiles from remote repository"
 git clone git@github.com:Jarmos-san/dotfiles ~/.dotfiles
 cd ~/.dotfiles
@@ -19,9 +27,6 @@ echo "Install necessary software as listed in Brewfile"
 brew bundle
 
 # TODO: Create symlinks using "stow"
-
-echo "Creating necessary folders"
-mkdir -p "projects" "work"
 
 # TODO: Setup GPG & SSH
 
