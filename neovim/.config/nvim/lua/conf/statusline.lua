0b672dd7b00370cae7c61176ed1b27ec0ae29fd5
Somraj Saha <somraj.mle@gmail.com>
2021-11-10 16:59:09 +0530

    :wrench: Add config file for custom statusline

---
+++ neovim/.config/nvim/lua/conf/statusline.lua
@@ -0,0 +1,8 @@
+-- Module for customising the default statusline.
+-- The "famui/feline.nvim" plugin is used to create, configure & customise the
+-- default statusline. For more info on it, refer to the following repository:
+-- https://github.com/famui/feline.nvim
+
+local M = {}
+
+return M
