4cb0bef9de4f6da423e9c47f8f88cdb79c0ff837
Somraj Saha <somraj.1994@gmail.com>
2022-02-02 22:31:49 +0530

    :fire: Remove code about generating SSH & GPG keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,6 @@ sudo apt-get install git curl
 echo "System update complete!"
 sleep 3
 
-read -p "Please provide a password for authenticating SSH connections: " -s password
-
 echo "Installing Homebrew"
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
@@ -30,28 +28,3 @@ echo "Install necessary software as listed in Brewfile"
 brew bundle
 
 # TODO: Create symlinks using "stow"
-
-# TODO: Setup GPG & SSH
-echo "Setting up SSH"
-ssh-keygen -t ed25519 -C somraj.mle@gmail.com -f ~/.ssh/id_ed25519 -N $password
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-
-cat > keyConfig <<EOF
-    Key-Type: default
-    Key-Length: 4096
-    Subkey-Type: default
-    Subkey-Length: 4096
-    Name-Real: Somraj Saha
-    Name-Comment: "GitHub GPG key for somraj.mle@gmail.com
-    Name-Email: somraj.mle@gmail.com
-    Expire-Date: 0
-    Protection: $password
-    %commit
-EOF
-
-echo "Generating GPG key...(this might take a while)"
-gpg --batch --generate-key keyConfig
-rm -rf keyConfig
-
-# TODO: Use GitHub CLI to setup SSH & GPG keys for the account
