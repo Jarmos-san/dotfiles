a83ba1d3f89f59fabfdd6cf2d651ab448292c9a6
Somraj Saha <somraj.1994@gmail.com>
2022-02-14 02:37:46 +0530

    :fire: Remove redundant Neovim config directory

--- neovim/.config/nvim/lua/conf/statusline.lua
+++
@@ -1,8 +0,0 @@
--- Module for customising the default statusline.
--- The "famui/feline.nvim" plugin is used to create, configure & customise the
--- default statusline. For more info on it, refer to the following repository:
--- https://github.com/famui/feline.nvim
-
-local M = {}
-
-return M
