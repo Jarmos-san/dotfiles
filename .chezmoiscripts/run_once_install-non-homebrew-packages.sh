eea8731ee6e079c380758ace70c385d5f99e37d0
Somraj Saha <somraj.1994@gmail.com>
2022-03-14 18:21:33 +0530

    :hammer: Add another script to install non-Homebrew apps

---
+++ .chezmoiscripts/run_once_install-non-homebrew-packages.sh
@@ -0,0 +1,7 @@
+# Following applications which aren't available in Homebrew needs to be installed:
+
+# 1. Microsoft Edge
+# 2. Visual Studio Code
+# 3. Inkscape
+# 4. Kitty Terminal
+# 5. Transmission Bittorrent client
