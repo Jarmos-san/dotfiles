f899a10326306aa51b49f72da7016f83971a23c7
Somraj Saha <somraj.1994@gmail.com>
2022-03-17 15:27:40 +0530

    :sparkles: Add the config for Neovim plugins

---
+++ dotfiles/private_dot_config/nvim/lua/core/plugins.lua
@@ -0,0 +1,34 @@
+--[[
+Module for installing plugins
+Add, remove or configure plugins as per your needs over here
+--]]
+
+local execute = vim.nvim_exec_command
+
+-- "packer.nvim" installation path
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+
+-- Ensure a local clone of "packer.nvim" exists
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    packer_bootstrap = vim.fn.system({
+        'git', 'clone', '--depth', '1',
+        'https://github.com/wbthomason/packer.nvim',
+        install_path
+    })
+end
+
+-- Load "packer.nvim"
+vim.cmd([[ packadd! packer.nvim ]])
+
+require('packer').startup(function(use)
+
+    use {
+        'wbthomason/packer.nvim',
+        opt = true
+    }
+
+    if packer_boostrap then
+        require('packer').sync()
+    end
+
+end)
