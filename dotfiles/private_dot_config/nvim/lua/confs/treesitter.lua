1898bdb75f8d88684788d7d79ba607cf373b410a
Somraj Saha <somraj.1994@gmail.com>
2022-03-17 16:24:50 +0530

    :recycle: Optimise the `nvim-treesitter` plugin configs

--- dotfiles/private_dot_config/nvim/lua/confs/treesitter.lua
+++ dotfiles/private_dot_config/nvim/lua/confs/treesitter.lua
@@ -4,22 +4,29 @@ For more reference checkout the official repository at:
 https://github.com/nvim-treesitter/nvim-treesitter
 --]]
 
-require('nvim-treesitter.configs').setup({
-    -- Ensure the following parsers are installed by default
-    ensure_installed = {
-        'lua', 'markdown', 'yaml', 'jsonc'
-    },
+local M = {}
 
-    -- Additional modules are also supported, to get a list of those, refer to
-    -- the docs available online at:
-    -- https://github.com/nvim-treesitter/nvim-treesitter#modules
+function M.config()
+    require('nvim-treesitter.configs').setup({
+        -- Ensure the following parsers are installed by default
+        ensure_installed = {
+            'lua', 'markdown', 'yaml', 'jsonc'
+        },
 
-    -- Feel free to also refer to ":h nvim-treesitter-modules"
-    highlight = {
-        enable = true
-    },
+        -- Additional modules are also supported, to get a list of those, refer to
+        -- the docs available online at:
+        -- https://github.com/nvim-treesitter/nvim-treesitter#modules
 
-    indent = {
-        enable = true
-    }
-})
+        -- Feel free to also refer to ":h nvim-treesitter-modules"
+        highlight = {
+            enable = true
+        },
+
+        indent = {
+            enable = true
+        }
+    })
+end
+
+
+return M
