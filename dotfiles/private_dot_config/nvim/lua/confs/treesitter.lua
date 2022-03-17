34c7ea9f19924366f54a5feaaa90c74723cf80e8
Somraj Saha <somraj.1994@gmail.com>
2022-03-17 16:06:01 +0530

    :wrench: Configure `nvim-treesitter`

---
+++ dotfiles/private_dot_config/nvim/lua/confs/treesitter.lua
@@ -0,0 +1,25 @@
+--[[
+Module for configuring the Treesitter plugin for Neovim
+For more reference checkout the official repository at:
+https://github.com/nvim-treesitter/nvim-treesitter
+--]]
+
+require('nvim-treesitter.configs').setup({
+    -- Ensure the following parsers are installed by default
+    ensure_installed = {
+        'lua', 'markdown', 'yaml', 'jsonc'
+    },
+
+    -- Additional modules are also supported, to get a list of those, refer to
+    -- the docs available online at:
+    -- https://github.com/nvim-treesitter/nvim-treesitter#modules
+
+    -- Feel free to also refer to ":h nvim-treesitter-modules"
+    highlight = {
+        enable = true
+    },
+
+    indent = {
+        enable = true
+    }
+})
