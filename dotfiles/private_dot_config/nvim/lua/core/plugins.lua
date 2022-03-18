f0db22028955994dff04fd001f5723dbeb3cea4c
Somraj Saha <somraj.1994@gmail.com>
2022-03-18 13:40:27 +0530

    :heavy_plus_sign: Add additionaal plugins for Neovim

--- dotfiles/private_dot_config/nvim/lua/core/plugins.lua
+++ dotfiles/private_dot_config/nvim/lua/core/plugins.lua
@@ -8,34 +8,43 @@ local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvi
 
 -- Ensure a local clone of "packer.nvim" exists
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	packer_bootstrap = vim.fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
+    packer_bootstrap = vim.fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
 end
 
 -- Load "packer.nvim"
 vim.cmd([[ packadd! packer.nvim ]])
 
 require("packer").startup(function(use)
-	use({
-		"wbthomason/packer.nvim",
-		opt = true,
-	})
+    use({
+        "wbthomason/packer.nvim",
+        opt = true,
+    })
 
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		config = function()
-			require("confs.treesitter").config()
-		end,
-	})
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        requires = {
+            {
+                "p00f/nvim-ts-rainbow",
+                after = "nvim-treesitter",
+            },
+        },
+        run = ":TSUpdate",
+        config = function()
+            require("confs.treesitter").config()
+        end,
+    })
 
-	if packer_boostrap then
-		require("packer").sync()
-	end
+    -- Use the "mini.nvim" for easier configurations.
+    -- More info is available at: https://github.com/echasnovski/mini.nvim
+
+    if packer_boostrap then
+        require("packer").sync()
+    end
 end)
