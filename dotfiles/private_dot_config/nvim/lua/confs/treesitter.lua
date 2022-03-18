3a3aeea096ef1c8d7481014b48385a7b462f20b7
Somraj Saha <somraj.1994@gmail.com>
2022-03-18 13:39:11 +0530

    :wrench: Configure the Neovim Treesitter for better colourised brackets

--- dotfiles/private_dot_config/nvim/lua/confs/treesitter.lua
+++ dotfiles/private_dot_config/nvim/lua/confs/treesitter.lua
@@ -2,33 +2,41 @@
 Module for configuring the Treesitter plugin for Neovim
 For more reference checkout the official repository at:
 https://github.com/nvim-treesitter/nvim-treesitter
+
+Additional plugins & modules are available at:
+https://github.com/nvim-treesitter/nvim-treesitter/wiki/Extra-modules-and-plugins
 --]]
 
 local M = {}
 
 function M.config()
-	require("nvim-treesitter.configs").setup({
-		-- Ensure the following parsers are installed by default
-		ensure_installed = {
-			"lua",
-			"markdown",
-			"yaml",
-			"jsonc",
-		},
+    require("nvim-treesitter.configs").setup({
+        -- Ensure the following parsers are installed by default
+        ensure_installed = {
+            "lua",
+            "markdown",
+            "yaml",
+            "jsonc",
+        },
 
-		-- Additional modules are also supported, to get a list of those, refer to
-		-- the docs available online at:
-		-- https://github.com/nvim-treesitter/nvim-treesitter#modules
+        -- Additional modules are also supported, to get a list of those, refer to
+        -- the docs available online at:
+        -- https://github.com/nvim-treesitter/nvim-treesitter#modules
 
-		-- Feel free to also refer to ":h nvim-treesitter-modules"
-		highlight = {
-			enable = true,
-		},
+        -- Feel free to also refer to ":h nvim-treesitter-modules"
+        highlight = {
+            enable = true,
+        },
 
-		indent = {
-			enable = true,
-		},
-	})
+        indent = {
+            enable = true,
+        },
+
+        -- Module powered by "p00f/nvim-ts-rainbow" module
+        rainbow = {
+            enable = true,
+        },
+    })
 end
 
 return M
