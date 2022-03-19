d29e32908e7f42c0e4f8ccae4d41f63fe6d51320
Somraj Saha <somraj.1994@gmail.com>
2022-03-19 20:08:44 +0530

    :heavy_plus_sign: Configure Neovim with a tree-based file explorer plugin

--- dotfiles/private_dot_config/nvim/lua/core/plugins.lua
+++ dotfiles/private_dot_config/nvim/lua/core/plugins.lua
@@ -53,6 +53,16 @@ require("packer").startup(function(use)
         },
     })
 
+    use({
+        "kyazdani42/nvim-tree.lua",
+        requires = {
+            "kyazdani42/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup({})
+        end,
+    })
+
     if packer_boostrap then
         require("packer").sync()
     end
