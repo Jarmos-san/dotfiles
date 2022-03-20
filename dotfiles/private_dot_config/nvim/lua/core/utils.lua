26e6ef880a57789871fb36c570c13411a2ee7b54
Somraj Saha <somraj.1994@gmail.com>
2022-03-20 14:10:47 +0530

    :poop: Clean up redundant code

--- dotfiles/private_dot_config/nvim/lua/core/utils.lua
+++ dotfiles/private_dot_config/nvim/lua/core/utils.lua
@@ -3,15 +3,16 @@ local M = {}
 local fn = vim.fn
 local cmd = vim.cmd
 
+-- TODO: Write a better more usable "autocommand" function
 -- Functional wrapper for creating AutoGroups
-function M.create_augroup(autocmds, name)
-    cmd("augroup " .. name)
-    cmd("autocmd!")
-    for _, autocmd in ipairs(autocmds) do
-        cmd("autocmd " .. table.concat(autocmd, " "))
-    end
-    cmd("augroup END")
-end
+-- function M.create_augroup(autocmds, name)
+--     cmd("augroup " .. name)
+--     cmd("autocmd!")
+--     for _, autocmd in ipairs(autocmds) do
+--         cmd("autocmd " .. table.concat(autocmd, " "))
+--     end
+--     cmd("augroup END")
+-- end
 
 -- Functional wrapper for mapping keys
 function M.map(mode, lhs, rhs, opts)
@@ -20,16 +21,6 @@ function M.map(mode, lhs, rhs, opts)
         options = vim.tbl_extend("force", options, opts)
     end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-function M.is_buffer_empty()
-    -- Check whether the current buffer is empty
-    return vim.fn.empty(vim.fn.expand("%:t")) == 1
-end
-
-function M.has_width_gt(cols)
-    -- Check if the windows width is greater than a given number of columns
-    return vim.fn.winwidth(0) / 2 > cols
 end
 
 return M
