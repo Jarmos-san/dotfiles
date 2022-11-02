462716bb4cb34acc407110aea1a27ec81bfa8fa5
Somraj Saha <somraj.1994@gmail.com>
2022-11-02 14:24:18 +0530

    :truck: Back up some more Neovim configurations

--- dotfiles/private_dot_config/nvim/lua/jarmos/plugins/lsp.lua
+++ dotfiles/private_dot_config/nvim/lua/jarmos/plugins/lsp.lua
@@ -87,7 +87,7 @@ function M.setup_lsp()
     settings = {
       yaml = {
         schemas = {
-          ["https://json.schemastore.org/github-workflow,json"] = ".github/workflows/*",
+          ["https://json.schemastore.org/github-action,json"] = ".github/workflows/*",
         },
       },
     },
