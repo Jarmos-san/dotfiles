afce629b425ed746f9031a5e5d345b8d93c551db
Somraj Saha <somraj.1994@gmail.com>
2022-12-26 15:31:35 +0530

    :fire: Cleanup some redundant directories

--- dotfiles/private_dot_config/private_fish/functions/ll.fish
+++
@@ -1,3 +0,0 @@
-function ll --wraps=ls --wraps=exa\ --long\ --all\ --classify\ --icons\ --git\ --ignore-glob=\'.git\' --description alias\ ll\ exa\ --long\ --all\ --classify\ --icons\ --git\ --ignore-glob=\'.git\'
-  exa --long --all --classify --icons --git --ignore-glob='.git' $argv;
-end
--- dotfiles/private_dot_config/private_fish/functions/loc.fish
+++
@@ -1,3 +0,0 @@
-function loc --wraps='wc -l' --description 'alias loc=wc -l'
-  wc -l $argv;
-end
