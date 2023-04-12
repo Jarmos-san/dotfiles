edaf09147cd16d95c6be4a31a36f8758efef3588
Somraj Saha <somraj.1994@gmail.com>
2023-04-12 17:39:52 +0530

    :construction_worker: Attempt to fix the CI/CD pipeline

--- setup.sh
+++ setup.sh
@@ -33,6 +33,8 @@ mkdir --parents "$KEYRINGS"
 chmod 0755 "$KEYRINGS"
 curl --fail --silent --show-error --location "$DOCKER_GPG_KEY" |
   gpg --dearmour -o "$KEYRINGS/docker.gpg"
+# Fix some annoying ShellCheck errors. See the docs for more info: https://www.shellcheck.net/wiki/SC1091
+# shellcheck source=/dev/null
 echo "deb [arch='$(dpkg --print-architecture)' signed-by=$KEYRINGS/docker.gpg] https://download.docker.com/linux/ubuntu \
   '$(. /etc/os-release && echo "$VERSION_CODENAME")' stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
