eaafa6a86c259be8e44bf43e4b5bc06388542874
Somraj Saha <somraj.1994@gmail.com>
2023-04-25 17:55:19 +0530

    :recycle: Reduce some redundancy from the Wezterm config file

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -1,39 +1,41 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-local config = {}
-
--- Some bare basic key bindings for managing the terminal
-config.keys = {
-  {
-    -- Key binding to toggle fullscreen mode
-    key = "F11",
-    mods = "NONE",
-    action = wezterm.action.ToggleFullScreen,
-  },
-}
-
--- Set the colorscheme to something which is usable
-config.color_scheme = "Catppuccin Mocha"
-
--- Set a monospaced font for easier time when writing code
-config.font = wezterm.font("CaskaydiaCove Nerd Font Mono", {
-  weight = "DemiBold",
-  stretch = "Normal",
-  style = "Normal",
-})
-
--- The default shell to invoke when launching the terminal
-config.default_prog = { "/home/linuxbrew/.linuxbrew/bin/zsh", "-l" }
-
--- Make the font size of the terminal contents legible
-config.font_size = 11.0
-
 -- Ensure the terminal is opened in maximised mode right after startup
 wezterm.on("gui-startup", function(cmd)
-  -- local tab, pane, window = mux.spawn_window(cmd or {})
   local _, _, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
 
-return config
+-- This Lua module returns a table of configuration values which Wezterm reads to
+-- customise the aesthetics of the terminal.
+return {
+  -- Reduce unnecessary padding on the window
+  window_padding = { left = 1, right = 0, top = 1, bottom = -4 },
+
+  -- Some bare basic key bindings for managing the terminal
+  keys = {
+    {
+      -- Key binding to toggle fullscreen mode
+      key = "F11",
+      mods = "NONE",
+      action = wezterm.action.ToggleFullScreen,
+    },
+  },
+
+  -- Set the colorscheme to something which is usable
+  color_scheme = "Catppuccin Mocha",
+
+  -- Set a monospaced font for easier time when writing code
+  font = wezterm.font("CaskaydiaCove Nerd Font Mono", {
+    weight = "DemiBold",
+    stretch = "Normal",
+    style = "Normal",
+  }),
+
+  -- The default shell to invoke when launching the terminal
+  default_prog = { "/home/linuxbrew/.linuxbrew/bin/zsh", "-l" },
+
+  -- Make the font size of the terminal contents legible
+  font_size = 11.0,
+}
