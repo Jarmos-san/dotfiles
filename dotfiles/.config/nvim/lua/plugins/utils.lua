a7ef133a0f04008406a98d8b778ec835a52ffa58
Somraj Saha <somraj.1994@gmail.com>
2023-04-26 16:39:23 +0530

    :recycle: Start moving some of the plugins to the "plugins.lua" file

--- dotfiles/.config/nvim/lua/plugins/utils.lua
+++ dotfiles/.config/nvim/lua/plugins/utils.lua
@@ -1,53 +1,6 @@
 -- Module for configuring some utilitarian plugins
 
--- Table of options for configuring the "smartcolumn" plugin
-local smartcolumn_options = {
-  disable_filetypes = {
-    "help",
-    "text",
-    "markdown",
-    "mason",
-    "lazy",
-    "lspinfo",
-    "alpha",
-    "checkhealth",
-    "Trouble",
-    "toml",
-    "zsh",
-    "gitignore",
-    "sh",
-    "json",
-    "tmux",
-    "gitconfig",
-    "gitattributes",
-  },
-  custom_colorcolumn = { lua = 120, dockerfile = 120, python = 88, yaml = 90, markdown = 80, typescriptreact = 81 },
-}
-
 return {
-  { "famiu/bufdelete.nvim" }, -- Plugin for deleting & removing buffers without messing up the window layout
-
-  { "max397574/better-escape.nvim", event = "BufReadPost" }, -- Plugin for a better & quicker "Escape" mechanism.
-
-  {
-    "karb94/neoscroll.nvim", -- Plugin to enable a smoother scroll animation
-    event = "BufReadPost", -- Load the plugin only after the contents of the buffer are read
-    config = function()
-      require("neoscroll").setup() -- Initialising the plugin with default configs is necessary for it work everytime
-    end,
-  },
-
-  {
-    "m4xshen/smartcolumn.nvim", -- Functionally better plugin for showing a nice colorcolum
-    event = "FileType", -- Load the plugin only when the filetype of the buffer is recognised.
-    opts = {
-      -- Disable the colorcolum in certain filetypes like Vim help files.
-      disabled_filetypes = smartcolumn_options.disable_filetypes,
-      -- Configure the character length at which to show the colorcolumn.
-      custom_colorcolumn = smartcolumn_options.custom_colorcolumn,
-    },
-  },
-
   {
     "norcalli/nvim-colorizer.lua", -- Plugin to display the colours of their respective hex codes.
     event = { "BufReadPost" },
