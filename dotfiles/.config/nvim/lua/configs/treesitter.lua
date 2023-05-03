a91018f05039605ea480c1c79143507533ba1afc
Somraj Saha <somraj.1994@gmail.com>
2023-05-03 18:43:15 +0530

    :poop: Remove some hacks related to the semantic highlighting

--- dotfiles/.config/nvim/lua/configs/treesitter.lua
+++ dotfiles/.config/nvim/lua/configs/treesitter.lua
@@ -51,32 +51,6 @@ local configs = {
 
 M.setup = function()
   require("nvim-treesitter.configs").setup(configs)
-  -- INFO: This is a stupid hack until the colorscheme plugin I use supports semantic highligting as well.
-  -- See this issue thread to keep track of semantic highlighting on the "navarasu/onedark.nvim" colorscheme;
-  -- https://github.com/navarasu/onedark.nvim
-
-  -- See this Reddit discussion for more information on the same:
-  -- https://www.reddit.com/r/neovim/comments/12gvms4/this_is_why_your_higlights_look_different_in_90
-  local links = {
-    ["@lsp.type.namespace"] = "@namespace",
-    ["@lsp.type.type"] = "@type",
-    ["@lsp.type.class"] = "@type",
-    ["@lsp.type.enum"] = "@type",
-    ["@lsp.type.interface"] = "@type",
-    ["@lsp.type.struct"] = "@structure",
-    ["@lsp.type.parameter"] = "@parameter",
-    ["@lsp.type.variable"] = "@variable",
-    ["@lsp.type.property"] = "@property",
-    ["@lsp.type.enumMember"] = "@constant",
-    ["@lsp.type.function"] = "@function",
-    ["@lsp.type.method"] = "@method",
-    ["@lsp.type.macro"] = "@macro",
-    ["@lsp.type.decorator"] = "@function",
-  }
-
-  for newgroup, oldgroup in pairs(links) do
-    vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
-  end
 end
 
 return M
