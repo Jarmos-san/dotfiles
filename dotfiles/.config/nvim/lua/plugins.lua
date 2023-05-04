96c6ae713539d6bba1173f7ca92b42f8cec912c2
Somraj Saha <somraj.1994@gmail.com>
2023-05-04 13:19:07 +0530

    :recycle: Move the LSP plugins to the plugin spec file as well

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -497,6 +497,55 @@ local plugins = {
       background_colour = "#000000",
     },
   },
+
+  {
+    -- Official plugin for more ease in configuring the in-built LSP client.
+    "neovim/nvim-lspconfig",
+    -- event = "BufReadPost", -- Lazy-load the plugin only after the Neovim UI is loaded.
+    config = function() -- Configurations for the many LSP servers used within Neovim.
+      require("configs.lsp")
+    end,
+    dependencies = {
+      -- This plugin needs to be loaded as well otherwise Neovim can't find the LSP binary on $PATH.
+      "williamboman/mason.nvim",
+    },
+  },
+
+  {
+    -- Plugin for VSCode-like snippets powered by Neovim's in-built LSP.
+    "L3MON4D3/LuaSnip",
+    event = "InsertEnter", -- Lazy-load the plugin only when the buffer is in Insert mode.
+    dependencies = { -- Load these dependencies when the snippet plugin is used.
+      "neovim/nvim-lspconfig",
+      "hrsh7th/nvim-cmp",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+
+  {
+    -- Extra plugin for a more VSCode-like snippets behaviour.
+    "rafamadriz/friendly-snippets",
+    event = "InsertEnter", -- Lazy-load the plugin only when the buffer is in an Insert mode.
+  },
+
+  {
+    -- Better autocompletion support for Neovim.
+    "hrsh7th/nvim-cmp",
+    dependencies = { -- A bunch of extra extensions for the autocompletions plugin.
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp-document-symbol",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function() -- Configuration module for the autocompletion module.
+      require("configs.cmp")
+    end,
+    event = "InsertEnter", -- Lazy-load the plugin only when the buffer is in Insert mode.
+  },
 }
 
 return plugins
