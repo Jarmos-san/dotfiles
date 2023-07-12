af9c0210060b308153893bf51dd62c980d7a4f8d
Somraj Saha <somraj.1994@gmail.com>
2023-07-12 11:49:24 +0530

    :poop: Cleanup some older bugg keymaps

--- dotfiles/.config/nvim/lua/keymaps.lua
+++ dotfiles/.config/nvim/lua/keymaps.lua
@@ -79,24 +79,16 @@ map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Navigate around windows more easily with the "hjkl" keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+map("n", "<leader>h", "<C-w>h", { desc = "Go to left window" })
+map("n", "<leader>j", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<leader>k", "<C-w>k", { desc = "Go to upper window" })
+map("n", "<leader>l", "<C-w>l", { desc = "Go to right window" })
 
 -- Resize windows using <Ctrl + Arrow keys>
 map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Increase window width" })
-
--- Move lines by pressing <Alt + hjkl keys>
-map("n", "<A-j>", "<cmd>m .=+1<cr>==", { desc = "Move lines down" })
-map("n", "<A-k>", "<cmd>m .=-2<cr>==", { desc = "Move lines up" })
-map("i", "<A-j>", "<esc><cmd>m .=+1<cr>==gi", { desc = "Move lines down" })
-map("i", "<A-k>", "<esc><cmd>m .=-2<cr>==gi", { desc = "Move lines up" })
-map("v", "<A-j>", "<cmd>m '>+1<cr>gv=gv", { desc = "Move lines down" })
-map("v", "<A-k>", "<cmd>m '<-2<cr>gv=gv", { desc = "Move lines up" })
 
 -- Clear the search highlight by pressing <ESC>
 map({ "i", "n" }, "<esc>", "<cmd>nohlsearch<cr><esc>", { desc = "Escape & clear highlighted search" })
