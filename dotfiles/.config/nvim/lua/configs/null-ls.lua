1572076c0f55851037d55b414584f0195d30690e
Somraj Saha <somraj.1994@gmail.com>
2023-07-15 15:45:54 +0530

    :recycle: Refactor the null-ls configuration in an attempt to fix it

--- dotfiles/.config/nvim/lua/configs/null-ls.lua
+++ dotfiles/.config/nvim/lua/configs/null-ls.lua
@@ -9,22 +9,29 @@ M.config = function()
   local formatting = null_ls.builtins.formatting
   local code_actions = null_ls.builtins.code_actions
 
+  -- Utility wrapper to make configuring the native vim.lsp.buf.format() function easier to use
+  local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+      filter = function(client)
+        -- apply whatever logic you want (in this example, we'll only use null-ls)
+        return client.name == "null-ls"
+      end,
+      bufnr = bufnr,
+    })
+  end
+
   -- Callback function to invoke after the Null-Ls is up & running
   local on_attach = function(client, bufnr)
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
     -- Check if the LSP supports formatting capabilities
     if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("formatter_group", { clear = true }),
+        group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({
-            timeout_ms = 6000,
-            bufnr = bufnr,
-            filter = function()
-              -- Configure Null-ls to perform the formatting instead of the LSP itself
-              return client.name == "null-ls"
-            end,
-          })
+          lsp_formatting(bufnr)
         end,
       })
     end
