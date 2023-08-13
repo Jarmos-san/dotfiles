75dfa15b96be20e143b4a74e8ac72a4ada58e5f1
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-08-13 18:57:02 +0530

    configure Neovim to apply some Markdown specific settings

---
+++ dotfiles/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+-- Filetype plugin which applies settings only for Markdown files
+
+vim.opt.wrap = true -- Wrap lines instead of continuing on beyond the limits of the monitor
+vim.opt.linebreak = true -- Wrap the lines at the end of a word instead of the last character of a line
