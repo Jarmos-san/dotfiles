3487abec06d492f3f9043a102133b1da51a660bf
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-08-13 19:26:11 +0530

    add a comment for future reference

--- dotfiles/.config/nvim/after/ftplugin/help.lua
+++ dotfiles/.config/nvim/after/ftplugin/help.lua
@@ -1,1 +1,4 @@
-vim.b.miniindentscope_disable = true -- This is necessary to disable indent lines on help files
+-- Settings to apply specifically to "vimdoc" files/buffers
+
+-- FIXME: Figure out how to configure the new "indent" plugin to be disabled on the said files
+-- vim.b.miniindentscope_disable = true -- This is necessary to disable indent lines on help files
