527b23f5d2d189956434d911dab7c08652bd5386
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-09-22 13:01:51 +0530

    update the statusline colour to use Tokyo Night

--- dotfiles/.config/nvim/lua/configs/lualine.lua
+++ dotfiles/.config/nvim/lua/configs/lualine.lua
@@ -7,7 +7,7 @@ M.config = function()
     -- Leaving an empty table renders the square-edged components, else the default angled ones are loaded
     section_separators = {},
     component_separator = "|",
-    theme = "onedark",
+    theme = "tokyonight",
     globalstatus = true,
     disabled_filetypes = {
       statusline = {
