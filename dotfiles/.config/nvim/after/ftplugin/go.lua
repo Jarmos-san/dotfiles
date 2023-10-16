aa671a1a58945f556f580ab0663aa8f7cdc2c1cc
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-10-16 14:22:14 +0530

    refactor the `gofmt` autocommand a bit

--- dotfiles/.config/nvim/after/ftplugin/go.lua
+++ dotfiles/.config/nvim/after/ftplugin/go.lua
@@ -7,8 +7,19 @@ autocmd("BufWritePost", {
   desc = "Format the buffer contents after save",
   group = augroup("format_buffer"),
   callback = function()
-    -- TODO: Figure a Lua native way to invoke the following command
-    -- INFO: Run the shell command with the current buffer in context
-    vim.cmd([[ silent %!gofmt % ]])
+    -- INFO: The command to format the contents of the buffer
+    local gofmt_command = "silent %!gofmt %"
+
+    -- INFO: Get the current location of the cursor on the current window
+    local cursor = vim.api.nvim_win_get_cursor(0)
+
+    -- INFO: The formatting command to invoke after the contents are saved
+    vim.cmd(gofmt_command)
+
+    -- INFO: In case the formatting got rid of the line we came from
+    cursor[1] = math.min(cursor[1], vim.api.nvim_buf_line_count(0))
+
+    -- INFO: Update the current cursor location according to the caluclated values
+    vim.api.nvim_win_set_cursor(0, cursor)
   end,
 })
