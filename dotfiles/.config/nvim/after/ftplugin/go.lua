365ecc849ae166de799a89dca02f7f1c667f036e
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-10-16 14:07:09 +0530

    remove some redundancy

--- dotfiles/.config/nvim/after/ftplugin/go.lua
+++ dotfiles/.config/nvim/after/ftplugin/go.lua
@@ -6,7 +6,6 @@ local augroup = require("utils").augroup
 autocmd("BufWritePost", {
   desc = "Format the buffer contents after save",
   group = augroup("format_buffer"),
-  pattern = { "*.go" },
   callback = function()
     -- TODO: Figure a Lua native way to invoke the following command
     -- INFO: Run the shell command with the current buffer in context
