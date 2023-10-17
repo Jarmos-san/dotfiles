4e06539b4e71970326538e00a8d3f617137c0385
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-10-17 12:37:47 +0530

    add/customise the special format logic

--- dotfiles/.config/nvim/after/ftplugin/go.lua
+++ dotfiles/.config/nvim/after/ftplugin/go.lua
@@ -2,6 +2,7 @@
 
 local autocmd = require("utils").autocmd
 local augroup = require("utils").augroup
+local format = require("utils").format
 
 autocmd("BufWritePost", {
   desc = "Format the buffer contents after save",
@@ -10,16 +11,7 @@ autocmd("BufWritePost", {
     -- INFO: The command to format the contents of the buffer
     local gofmt_command = "silent %!gofmt %"
 
-    -- INFO: Get the current location of the cursor on the current window
-    local cursor = vim.api.nvim_win_get_cursor(0)
-
-    -- INFO: The formatting command to invoke after the contents are saved
-    vim.cmd(gofmt_command)
-
-    -- INFO: In case the formatting got rid of the line we came from
-    cursor[1] = math.min(cursor[1], vim.api.nvim_buf_line_count(0))
-
-    -- INFO: Update the current cursor location according to the caluclated values
-    vim.api.nvim_win_set_cursor(0, cursor)
+    -- INFO: Invoke the formatting command with special logic
+    format(gofmt_command)
   end,
 })
--- dotfiles/.config/nvim/after/ftplugin/python.lua
+++ dotfiles/.config/nvim/after/ftplugin/python.lua
@@ -3,7 +3,11 @@
 -- https://github.com/aikow/dotfiles/blob/main/config/nvim/after/ftplugin/python.lua
 
 local opt = vim.opt
+
+local autocmd = require("utils").autocmd
+local augroup = require("utils").augroup
 local map = require("utils").map
+local format = require("utils").format
 
 opt.expandtab = true
 opt.autoindent = true
@@ -14,3 +18,16 @@ opt.softtabstop = 4
 
 -- INFO: Local keymap (specific to Python files) to execute the current Python script
 map("n", "<F5>", "<CMD>terminal python3 %<CR>")
+
+autocmd("BufWritePost", {
+  desc = "Format buffer contents after writing them",
+  group = augroup("format_python_files"),
+  callback = function()
+    -- INFO: Command to invoke black from within Neovim
+    local black_command = "silent %!black - --quiet"
+
+    -- TODO: Create logic to identify the venv and only then invoke the following command
+    -- INFO: Invoke the formatting command with special logic
+    format(black_command)
+  end,
+})
