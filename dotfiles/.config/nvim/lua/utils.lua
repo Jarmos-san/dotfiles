078066eeb45634d6d4589224080012ea8f8482d2
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-10-17 12:37:25 +0530

    add utility function to format source code universally

--- dotfiles/.config/nvim/lua/utils.lua
+++ dotfiles/.config/nvim/lua/utils.lua
@@ -19,4 +19,18 @@ M.map = function(mode, lhs, rhs, opts)
   end
 end
 
+M.format = function(command)
+  -- INFO: Get the current location of the cursor on the current window
+  local cursor = vim.api.nvim_win_get_cursor(0)
+
+  -- INFO: The formatting command to invoke after the contents are saved
+  vim.cmd(command)
+
+  -- INFO: In case the formatting got rid of the line we came from
+  cursor[1] = math.min(cursor[1], vim.api.nvim_buf_line_count(0))
+
+  -- INFO: Update the current cursor location according to the caluclated values
+  vim.api.nvim_win_set_cursor(0, cursor)
+end
+
 return M
