4bd44faef3aab0fd4274c7f14d7af534e84d90e7
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-11-28 22:25:32 +0530

    disable word wrapping for TSX files

--- dotfiles/.config/nvim/after/ftplugin/typescriptreact.lua
+++ dotfiles/.config/nvim/after/ftplugin/typescriptreact.lua
@@ -1,3 +1,1 @@
 vim.opt.iskeyword:append("-") -- Make Neovim recognise dash-seperated words as a single word
-vim.opt.wrap = true -- Ensure long running lines are wrapped for better legibility
-vim.opt.linebreak = true -- Ensure the lines are wrapped according to the words and not to a character length
