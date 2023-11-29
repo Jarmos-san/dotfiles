0778425661fec2e9f5e5b1eb20b2a6b75871a277
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-11-29 15:57:39 +0530

    add and refactor the smartcolumn plugin

---
+++ dotfiles/.config/nvim/lua/plugins/smartcolumn.lua
@@ -0,0 +1,40 @@
+-- Module for configuring the colour column smartly
+
+return {
+  "m4xshen/smartcolumn.nvim",
+  event = "BufRead",
+  opts = {
+    disabled_filetypes = {
+      "checkhealth",
+      "dosini",
+      "dotenv",
+      "gitattributes",
+      "gitconfig",
+      "gitignore",
+      "help",
+      "json",
+      "markdown",
+      "mason",
+      "mdx",
+      "lazy",
+      "log",
+      "lspinfo",
+      "sh",
+      "starter",
+      "text",
+      "Trouble",
+      "tmux",
+      "toml",
+      "zsh",
+    },
+    custom_colorcolumn = {
+      lua = 120,
+      dockerfile = 120,
+      python = 88,
+      yaml = 90,
+      markdown = 80,
+      typescriptreact = 81,
+      rust = 100,
+    },
+  },
+}
