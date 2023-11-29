11ef0f5903f96e5d02a214b97ab95847662af946
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-11-30 00:20:09 +0530

    update the LSP names for automatic installation

--- dotfiles/.config/nvim/lua/plugins/mason.lua
+++ dotfiles/.config/nvim/lua/plugins/mason.lua
@@ -1,49 +1,58 @@
 -- Module for configuring the "mason" plugin for managing LSP servers
 
 return {
-  "williamboman/mason.nvim",
-  cmd = { "LspInstall", "LspUninstall", "Mason" },
-  opts = {
-    -- Configure the plugin to have rounded borders
-    ui = { border = "rounded" },
+  {
+    "williamboman/mason.nvim",
+    cmd = { "LspInstall", "LspUninstall", "Mason" },
+    opts = {
+      -- Configure the plugin to have rounded borders
+      ui = { border = "rounded" },
 
-    -- Configure the log levels for the plugin
-    log_level = vim.log.levels.WARN,
-
-    -- LSP servers to ensure is always installed
-    ensure_installed = {
-      -- Some of the commented out LSP servers aren't available for download!
-      "astro",
-      "bashls",
-      -- "black",
-      -- "debugpy",
-      "cssls",
-      "dockerls",
-      "eslint",
-      "gopls",
-      "html",
-      "jsonls",
-      "lua_ls",
-      -- "prettier",
-      "pyright",
-      "ruff_lsp",
-      "rust_analyzer",
-      -- "taplo", -- TODO: Figure out how to make it identify the "pyproject.toml" file
-      -- "selene", -- FIXME: Broken on Ubuntu 20.04
-      -- "shellcheck",
-      -- "shfmt",
-      -- "stylua",
-      "stylelint_lsp",
-      "sqlls",
-      "tailwindcss",
-      "tsserver",
-      "typst_lsp",
-      "vale_ls",
-      "yamlls",
+      -- Configure the log levels for the plugin
+      log_level = vim.log.levels.WARN,
     },
+    config = function(_, opts)
+      require("mason").setup(opts)
+    end,
+    dependencies = { "neovim/nvim-lspconfig" },
   },
-  config = function(_, opts)
-    require("mason").setup(opts)
-  end,
-  dependencies = { "neovim/nvim-lspconfig", "williamboman/mason-lspconfig.nvim" },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    cmd = { "MasonToolsInstall", "MasonToolsUpdate", "MasonToolsClean" },
+    opts = {
+      ensure_installed = {
+        "astro-language-server",
+        "bash-language-server",
+        "black",
+        "debugpy",
+        "css-lsp",
+        "dockerfile-language-server",
+        "eslint-lsp",
+        "gopls",
+        "hadolint",
+        "html-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "prettier",
+        "pyright",
+        "ruff",
+        -- "taplo", -- TODO: Figure out how to make it identify the "pyproject.toml" file
+        "selene", -- FIXME: Broken on Ubuntu 20.04
+        "shellcheck",
+        "shfmt",
+        "stylua",
+        "stylelint",
+        "sqlls",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "typst-lsp",
+        "vale-ls",
+        "yaml-language-server",
+      },
+    },
+    config = function(_, opts)
+      require("mason-tool-installer").setup(opts)
+    end,
+  },
 }
