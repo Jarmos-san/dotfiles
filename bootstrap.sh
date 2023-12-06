a9de6e2f347a8f54e7e78a4665718035d00bc08d
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-06 12:03:10 +0530

    recreate the base of the `bootstrap` Shell script

---
+++ bootstrap.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Script to automatically setup a fresh new Linux/MacOS system (or VM).
+#
+# This script should ONLY be used after installing a fresh new OS on a new
+# machine or inside a VM to setup a development environment automatically. The
+# script will install necessary tools like code editors, package managers and
+# more before setting up their configuration files automatically.
+#
+# USAGE: To use the script, run the following command (its not ready for usage yet);
+#
+# curl -fsSL https://raw.githubusercontent.com/<username>/<repository>/path/to/script \
+#   | bash -c
+#
+# NOTE: The script is designed to be subjective, hence it is RECOMMENDED to NOT
+# invoke it without understanding the context of the contents! Please read
+# through the script or reach out to the author for clarification on what
+# certain parts of the script does.
+#
+# DISCLAIMER: The script IS NOT TESTED and there is no guranteed it works
+# properly!
+#
+# Author: Somraj Saha <somraj.saha@jarmos.dev>
+# License: MIT (see the LICENSE document for more info on this regards)
+###############################################################################
+
+# Enable some options for Bash to stop executing the script if there's an error
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Enable debugging capabilities to the script
+if [[ "${TRACE-0}" == "1" ]]; then
+  set -o xtrace
+fi
+
+# Logic to print out a simple help message to STDOUT
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+  echo 'Usage: ./script.sh arg-one arg-two
+
+This is an awesome bash script to make your life better.
+
+'
+  exit
+fi
+
+# Execute the script in context to the current directory where the script is run
+cd "$(dirname "$0")"
+#
+# Message labels with appropriate terminal colours to showcase its importance
+ERROR="\033[0;31m[ERROR]\033[0m"
+SUCCESS="\033[0;32m[SUCCESS]\033[0m"
+WARNING="\033[0;33m[WARNING]\033[0m"
+INFO="\033[0;34m[INFO]\033[0m"
+
+###############################################################################
+# Print a nicely formatted informative message to STDOUT
+###############################################################################
+info() {
+  echo -e "$INFO $1"
+}
+
+###############################################################################
+# Print a nicely formatted warning message to STDOUT
+###############################################################################
+warning() {
+  echo -e "$WARNING $1"
+}
+
+###############################################################################
+# Print a nicely formatted success message to STDOUT
+###############################################################################
+success() {
+  echo -e "$SUCCESS $1"
+}
+
+###############################################################################
+# Print a nicely formatted error message to STDOUT
+###############################################################################
+error() {
+  echo -e "$ERROR $1"
+}
+
+###############################################################################
+# Cleanup any dangling variables or artifacts post script execution
+###############################################################################
+cleanup() {
+  unset ERROR
+  unset SUCCESS
+  unset WARNING
+  unset INFO
+}
+
+###############################################################################
+# The entrypoint of the script which will run the script as per the prescribed
+# logic
+###############################################################################
+main() {
+  error "Hello World!"
+
+  # Run the cleanup function after all the logic above has run without any
+  # errors!
+  cleanup
+}
+
+# Defer running the script till the last moment for safety reasons
+main "$@"
