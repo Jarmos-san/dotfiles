779ac755f38edcfd458dfc2d4d76ffb7a9a0eaa2
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-12 18:09:37 +0530

    remove redundant autocommands

--- dotfiles/.config/nvim/after/ftplugin/lua.lua
+++ dotfiles/.config/nvim/after/ftplugin/lua.lua
@@ -3,14 +3,6 @@
 local autocmd = require("utils").autocmd
 local augroup = require("utils").augroup
 
-autocmd("BufWritePre", {
-  desc = "Format Lua files using Stylua",
-  group = augroup("format_lua_files"),
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
-})
-
 autocmd("BufWritePost", {
   desc = "Lint Lua files using Stylua",
   group = augroup("lint_lua_files"),
