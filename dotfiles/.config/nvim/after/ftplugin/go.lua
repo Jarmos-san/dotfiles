7b38608fe3f349d17044ce690481d804c2f8ca98
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-12-26 15:21:21 +0530

    chore: configure Neovim to format Golang filetypes

--- dotfiles/.config/nvim/after/ftplugin/go.lua
+++ dotfiles/.config/nvim/after/ftplugin/go.lua
@@ -2,22 +2,13 @@
 
 local autocmd = require("utils").autocmd
 local augroup = require("utils").augroup
-local format = require("utils").format
 local map = require("utils").map
 
 autocmd("BufWritePost", {
   desc = "Format the buffer contents after save",
-  group = augroup("format_buffer"),
+  group = augroup("format_go_files"),
   callback = function()
-    -- INFO: The command to format the contents of the buffer
-    local gofmt_command = "silent %!gofmt %"
-
-    -- INFO: Invoke the formatting command with special logic
-    format(gofmt_command)
-
-    if not vim.g.shell_error == 0 then
-      vim.cmd("undo")
-    end
+    require("lint").try_lint()
   end,
 })
 
