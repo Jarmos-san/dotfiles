c9801126e03532634813490bf27fa12edb389ace
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-03-25 14:20:16 +0530

    refactor the the Vim options to be more legible

--- dotfiles/.config/nvim/lua/options.lua
+++ dotfiles/.config/nvim/lua/options.lua
@@ -1,52 +1,106 @@
 local opt = vim.opt
 
-opt.clipboard = "unnamedplus" -- Sync the system clipboard with Neovim
-opt.laststatus = 3 -- Enable the global status line
-opt.completeopt = "menu,menuone,noselect" -- Make the autocompletion window more accessible
-opt.conceallevel = 3 -- Hide the * markups for bold & italics
--- opt.expandtab = true -- Use spaces instead of tabs
--- TODO: Figure out what purpose does it serve
--- opt.formatoptions = "jcroqlnt" -- Some formatting configuration, needs more research
-opt.ignorecase = true -- Ignore casing when searching for a word
-opt.inccommand = "nosplit" -- Preview incremental substitute
--- opt.shiftround = true -- Roundup indentations
--- TODO: Figure a way out to configure these options specific to the filetype instead
--- opt.shiftwidth = 2 -- The default size of an indentation (this will be configure per filetype later on)
--- opt.tabstop = 2 -- Number of Spaces, a single tab counts for
-opt.smartcase = true -- Don't ignore casing with capitals
-opt.spelllang = { "en" } -- The language to use when spelling checking
-opt.splitbelow = true -- Put new windows below current
-opt.splitright = true -- Put new windows right of the current
-opt.timeoutlen = 300 -- ??
-opt.undofile = true -- ??
-opt.undolevels = 10000 -- ??
-opt.updatetime = 200 -- Save swap file & trigger CursorHold
-opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winminwidth = 5 -- Maximum window width
-opt.swapfile = false --Disable swapfile creation since it gets annoying after a while
+-- Sync the system clipboard with Neovim
+opt.clipboard = "unnamedplus"
 
--- INFO: Apply some aesthetic improvements to the UI/UX
-opt.cursorline = true -- Configure Neovim to highlight the current location of the cursor
-opt.termguicolors = true -- True color support for various plugins
-opt.pumblend = 10 -- Make the completion popup window a bit transparent
-opt.pumheight = 10 -- Display only 10 rows for the completion menu
+-- Enable the global status line
+opt.laststatus = 3
 
--- The following options are enabled for performance improvement reasons
--- INFO: Found these settings in the documentations (scroll down a bit in the ":h wrap-off" section)
-opt.showmode = false -- Disable showing the current mode since its already visible on the statusline
-opt.showcmd = false -- Disable prompting the invoked command back to the user
-opt.ruler = false -- Disable showing the current location of the cursor since the Lualine plugin takes care of it
+-- Make the autocompletion window more accessible
+opt.completeopt = "menu,menuone,noselect"
 
--- INFO: These are some QoL improvements to make writing code so much easier!
-opt.wrap = false -- Configure Neovim to not wrap the contents of the buffer
-opt.autowrite = true -- Save the contents of the buffer after exiting Insert mode
-opt.confirm = true -- Prompt to save the contents of the buffer before exiting
-opt.showbreak = "...." -- Add a slight padding to wrapped lines to distinguish them from the rest of the lines
-opt.sidescroll = 8 -- Enable horinzontal scrolling when word wrap is disabled
+-- Hide the * markups for bold & italics
+opt.conceallevel = 3
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Preview substitutions as its typed
+opt.inccommand = "split"
+
+-- The language to use when spelling checking
+opt.spelllang = { "en" }
+
+-- Configure how new splits should be opened
+opt.splitbelow = true
+opt.splitright = true
+
+-- Decrease mapped sequence wait time to display which-key popup sooner
+opt.timeoutlen = 300
+
+-- Save undo history
+opt.undofile = true
+opt.undolevels = 10000
+
+-- Save swap file & trigger CursorHold
+opt.updatetime = 200
+
+-- Command-line completion mode
+opt.wildmode = "longest:full,full"
+
+-- Maximum window width
+opt.winminwidth = 5
+
+--Disable swapfile creation since it gets annoying after a while
+opt.swapfile = false
+
+-- Configure Neovim to highlight the current location of the cursor
+opt.cursorline = true
+
+-- True color support for various plugins
+opt.termguicolors = true
+
+-- Make the completion popup window a bit transparent
+opt.pumblend = 10
+
+-- Display only 10 rows for the completion menu
+opt.pumheight = 10
+
+-- Disable showing the current mode since its already visible on the statusline
+opt.showmode = false
+
+-- Disable prompting the invoked command back to the user
+opt.showcmd = false
+
+-- Disable showing the current location of the cursor since the Lualine plugin takes care of it
+opt.ruler = false
+
+-- Configure Neovim to not wrap the contents of the buffer
+opt.wrap = false
+
+-- Add a slight padding to wrapped lines to distinguish them from the rest of the lines
+opt.showbreak = "...."
+
+-- Save the contents of the buffer after exiting Insert mode
+opt.autowrite = true
+
+-- Prompt to save the contents of the buffer before exiting
+opt.confirm = true
+
+-- Enable horinzontal scrolling when word wrap is disabled
+opt.sidescroll = 8
+
 -- FIXME: The following option might only be useful in certain filetypes like JSX, Markdown and such
-opt.iskeyword:append("-") -- Make Neovim recognise dash-seperated words as a single word
-opt.scrolloff = 6 -- Maintain a buffer of rows between the current row & the either ends of the window
-opt.number = true -- Enable the current line number to be shown
-opt.relativenumber = true -- Display the row number relative to the up/down rows
-opt.signcolumn = "yes" -- Allow the sign column to show else it'll keep shifting later on
-opt.shell = "zsh" -- Explicitly tell Neovim to use ZSH for terminal commands instead of relying on $SHELL
+-- Make Neovim recognise dash-seperated words as a single word
+opt.iskeyword:append("-")
+
+-- Maintain a buffer of rows between the current row & the either ends of the window
+opt.scrolloff = 6
+
+-- Enable the current line number to be shown
+opt.number = true
+opt.relativenumber = true
+
+-- Allow the sign column to show else it'll keep shifting later on
+opt.signcolumn = "yes"
+
+-- Explicitly tell Neovim to use ZSH for terminal commands instead of relying on $SHELL
+opt.shell = "zsh"
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
