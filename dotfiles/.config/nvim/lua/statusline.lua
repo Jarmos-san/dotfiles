e36b6ac5994d3d37ed093ec521323f22f763783a
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-05-06 16:59:23 +0530

    chore: add some referential resources for the future

--- dotfiles/.config/nvim/lua/statusline.lua
+++ dotfiles/.config/nvim/lua/statusline.lua
@@ -1,5 +1,7 @@
 -- This is a WORK-IN-PROGRESS custom implementation of a statusline.
 -- Once complete, it'll replace the lualine plugin
+-- Inspirational resources are as follows:
+-- https://nuxsh.is-a.dev/blog/custom-nvim-statusline.html
 
 local M = {}
 
