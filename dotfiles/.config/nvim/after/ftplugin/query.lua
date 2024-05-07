8a2a4e59c496cfc3997c31df75f955d915623213
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-05-07 08:57:07 +0530

    chore: disable the number column for `query` buffer types

--- dotfiles/.config/nvim/after/ftplugin/query.lua
+++ dotfiles/.config/nvim/after/ftplugin/query.lua
@@ -1,3 +1,5 @@
 -- Filetype plugin configurations for the "query" filetype
 
-vim.opt.relativenumber = false -- Disable the redundant number column on the right side for "query" files
+-- Disable the redundant number column on the right side for "query" files
+vim.wo.number = false
+vim.wo.relativenumber = false
