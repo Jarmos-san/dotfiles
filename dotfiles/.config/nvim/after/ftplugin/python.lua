08f37953d88d9b4582c63d2b0575481a6a317680
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-11-22 17:29:59 +0530

    chore: remove a redundant autocommand for Python files

--- dotfiles/.config/nvim/after/ftplugin/python.lua
+++ dotfiles/.config/nvim/after/ftplugin/python.lua
@@ -5,7 +5,6 @@
 local autocmd = require("utils").autocmd
 local augroup = require("utils").augroup
 local map = require("utils").map
-local format = require("utils").format
 
 -- INFO: Local keymap (specific to Python files) to execute the current Python script
 if os.getenv("VIRTUAL_ENV") ~= nil then
@@ -14,27 +13,6 @@ if os.getenv("VIRTUAL_ENV") ~= nil then
 else
   map("n", "<F5>", "<CMD>terminal python3 %<CR>")
 end
-
-autocmd("BufWritePost", {
-  desc = "Format buffer contents after writing them",
-  group = augroup("format_python_files"),
-  callback = function()
-    -- INFO: Command to invoke black from within Neovim
-    local black_command = "silent !black % --stdin-filename % --quiet"
-
-    -- INFO: Run the formatting command on the buffer contents if the venv is activated
-    if os.getenv("VIRTUAL_ENV") == nil then
-      vim.notify("[WARN] Please activate the virtual environment first!", vim.log.levels.WARN)
-    else
-      format(black_command)
-    end
-
-    -- INFO: If "black" exited with an error, undo the changes to the previous state
-    if not vim.g.shell_error == 0 then
-      vim.cmd("undo")
-    end
-  end,
-})
 
 autocmd({ "BufWritePost" }, {
   desc = "Lint buffer contents after writing them",
