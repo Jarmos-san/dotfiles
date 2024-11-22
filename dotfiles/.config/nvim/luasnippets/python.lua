120192bfa13d1d290730a0f7b7dd3a86be21b565
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-11-22 22:02:09 +0530

    chore: refactor and move the Python snippets to its own module (fixes #17)

---
+++ dotfiles/.config/nvim/luasnippets/python.lua
@@ -0,0 +1,127 @@
+local luasnip = require("luasnip")
+local snippet = luasnip.snippet
+local insert_node = luasnip.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+  -- Define the snippet for Python function definitions
+  snippet(
+    { trig = "def", desc = "Create a Python function definition" },
+    fmt( -- The format of the snippet
+      [[
+      def {1}({2}: {3}) -> {4}:
+          """{5}.
+
+          Args:
+            {2}: {6}.
+
+          Returns:
+            {4}: {7}
+
+          Raises:
+            {8}
+          """
+          {9}
+      ]],
+      { -- The node of the snippet to replace content with
+        insert_node(1, "function_name"),
+        insert_node(2, "arg"),
+        insert_node(3, "ArgumentType"),
+        insert_node(4, "ReturnType"),
+        insert_node(5, "Describe the function briefly."),
+        insert_node(6, "Explain the argument briefly."),
+        insert_node(7, "Explain the return type briefly."),
+        insert_node(8, "Exception"),
+        insert_node(9, ""),
+      },
+      {
+        repeat_duplicates = true, -- Ensure duplicates content are copied as-is
+      }
+    )
+  ),
+
+  -- Define the snippet for Python classes
+  snippet(
+    { trig = "class", desc = "Create a Python class implementation" },
+    fmt(
+      [[
+        class {1}({2}):
+            """{3}"""
+
+            {4}
+        ]],
+      {
+        insert_node(1, "ClassName"),
+        insert_node(2, "ParentClassName"),
+        insert_node(3, "Briefly describe the class and its characteristics."),
+        insert_node(4, ""),
+      }
+    )
+  ),
+
+  -- Snippet for the "if __name__ == '__main__:' logic"
+  snippet(
+    { trig = "__main__", desc = "Create logic to invoke the Python script with apt shebang" },
+    fmt(
+      [[
+  if __name__ == '__main__':
+      main()
+  ]],
+      {}
+    )
+  ),
+
+  -- Snippet for the "__repr__" function
+  snippet(
+    { trig = "__repr__", desc = "Implement a class's __repr__ method" },
+    fmt(
+      [[
+  def __repr__(self) -> str:
+      """{1}"""
+      return f"{{{2}}}"
+  ]],
+      {
+        insert_node(1, "Return a string representation of the class instance for debugging."),
+        insert_node(2, ""),
+      }
+    )
+  ),
+
+  -- Snippet for if-else statements
+  snippet(
+    { trig = "if-else", desc = 'Create an "if-else" logic block' },
+    fmt(
+      [[
+  if {1}:
+      {2}
+  else:
+      {3}
+  ]],
+      {
+        insert_node(1, ""),
+        insert_node(2, ""),
+        insert_node(3, ""),
+      }
+    )
+  ),
+
+  -- Snippets for match-case statements
+  snippet(
+    { trig = "match", desc = "Create a match statement" },
+    fmt(
+      [[
+  match {1}:
+      case {2}:
+          {3}
+      case _:
+          {4}
+  ]],
+      {
+        insert_node(1, ""),
+        insert_node(2, ""),
+        insert_node(3, ""),
+        insert_node(4, ""),
+      }
+    )
+  ),
+}
