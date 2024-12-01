2bf264251d2b79633fe1040277a896a930d42cc4
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-12-01 16:48:59 +0530

    chore: add another snippet for Vue files

--- dotfiles/.config/nvim/luasnippets/vue.lua
+++ dotfiles/.config/nvim/luasnippets/vue.lua
@@ -4,6 +4,7 @@ local insert_node = luasnip.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
+  -- Snippet to create the script template
   snippet(
     { trig = "script", desc = "Create a script block" },
     fmt(
@@ -11,6 +12,19 @@ return {
   <script lang='ts' setup>
   {1}
   </script>
+  ]],
+      { insert_node(1, "") }
+    )
+  ),
+
+  -- Snippet for creating the style template
+  snippet(
+    { trig = "style", desc = "Create a style template block" },
+    fmt(
+      [[
+  <style lang='scss' scoped>
+  {1}
+  </style>
   ]],
       { insert_node(1, "") }
     )
