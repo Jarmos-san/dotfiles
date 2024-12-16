27334ba95ffd2504877df0016c519043867f9285
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-12-16 19:17:16 +0530

    chore: configure the LSP to work with Ansible

--- dotfiles/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/lspconfig.lua
@@ -171,6 +171,12 @@ return {
       on_attach = on_attach,
       capabilities = capabilities,
     })
+
+    -- Enable the LSP for Ansible work
+    lspconfig["ansiblels"].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
   end,
   dependencies = {
     -- This plugin needs to be loaded as well otherwise Neovim can't find the LSP binary on $PATH.
