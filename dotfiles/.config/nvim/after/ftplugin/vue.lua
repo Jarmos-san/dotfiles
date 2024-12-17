ae51dec8d61b71f833395a1cf72fc2a6d776b4a0
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-12-17 14:13:34 +0530

    chore: configure Neovim to fold at higher indents

--- dotfiles/.config/nvim/after/ftplugin/vue.lua
+++ dotfiles/.config/nvim/after/ftplugin/vue.lua
@@ -1,4 +1,4 @@
 -- Folding configurations for Neovim
 vim.o.foldmethod = "expr" -- Allow folding using the "nvim-treesitter" plugin
 vim.o.foldexpr = "nvim_treesitter#foldexpr()" -- Configure the folding algorithm to be from the plugin
-vim.o.foldlevel = 1 -- Configure the maximum indenting for the folding
+vim.o.foldlevel = 0 -- Configure the maximum indenting for the folding
