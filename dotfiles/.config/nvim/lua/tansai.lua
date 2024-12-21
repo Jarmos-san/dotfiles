53e5e8bb8f27d7d0a68e41bb60c99e91bd840c54
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-12-21 17:39:09 +0530

    chore: make the Tansai the default customised colourscheme for myself

--- dotfiles/.config/nvim/lua/tansai.lua
+++ dotfiles/.config/nvim/lua/tansai.lua
@@ -1,17 +1,36 @@
 local M = {}
 
+-- Alias to the API for setting the syntax highlights
+local hl = vim.api.nvim_set_hl
+
+-- Table of colours
+local colours = {
+  none = "none",
+  grey = "#45413d",
+  light_grey = "#6e6c69",
+  white = "#ffffff",
+  black = "#000000",
+  blue = "blue",
+}
+
 M.load = function()
+  -- Clear any syntax highlighting (if any) that exists
   if vim.g.colors_name then
     vim.cmd.hi("clear")
   end
 
+  -- Set some basic configuration options for Neovim
   vim.g.colors_name = "tansai"
   vim.o.termguicolors = true
   vim.o.background = "dark"
 
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none", fg = "none" })
-  vim.api.nvim_set_hl(0, "NonText", { bg = "none", fg = "none" })
-  vim.api.nvim_set_hl(0, "CursorLine", { bg = "none", fg = "none" })
+  -- Basic highlighting unrelated to code (or syntax) highlights
+  hl(0, "Normal", { bg = colours.none, fg = colours.none })
+  hl(0, "NonText", { bg = colours.none, fg = colours.none })
+  hl(0, "CursorLine", { bg = colours.grey, fg = colours.none })
+  hl(0, "CursorLineNr", { bg = colours.grey, fg = colours.white })
+  hl(0, "LineNr", { bg = colours.none, fg = colours.grey })
+  hl(0, "Comment", { bg = colours.none, fg = colours.light_grey })
 end
 
 return M
