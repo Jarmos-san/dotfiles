606d86b9d5e0646d18a283bfaef845cf43e60d0b
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-12-23 17:43:34 +0530

    chore: conditionally apply folding configurations for Vue files

--- dotfiles/.config/nvim/after/ftplugin/vue.lua
+++ dotfiles/.config/nvim/after/ftplugin/vue.lua
@@ -1,4 +1,6 @@
 -- Folding configurations for Neovim
-vim.o.foldmethod = "expr" -- Allow folding using the "nvim-treesitter" plugin
-vim.o.foldexpr = "nvim_treesitter#foldexpr()" -- Configure the folding algorithm to be from the plugin
-vim.o.foldlevel = 0 -- Configure the maximum indenting for the folding
+if vim.api.nvim_buf_line_count(0) >= 100 then
+  vim.o.foldmethod = "expr" -- Allow folding using the "nvim-treesitter" plugin
+  vim.o.foldexpr = "nvim_treesitter#foldexpr()" -- Configure the folding algorithm to be from the plugin
+  vim.o.foldlevel = 1 -- Configure the maximum indenting for the folding
+end
