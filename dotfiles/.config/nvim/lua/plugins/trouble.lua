da5c06c1bc1c7e9798da4df4116937b04030180a
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-01-11 03:36:34 +0530

    chore: remove unnecessary plugins

--- dotfiles/.config/nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
--- Module for configuring the "trouble.nvim" plugin
-
-return {
-  "folke/trouble.nvim",
-  event = "LspAttach",
-  cmd = "Trouble",
-  config = true,
-  dependencies = "devicons",
-}
