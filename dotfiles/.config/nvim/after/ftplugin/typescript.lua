82946e0a4aff21dcc5a94ab18cf4867ecf40acd4
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-01-16 15:14:53 +0530

    chore: add a filetype plugin for TypeScript

---
+++ dotfiles/.config/nvim/after/ftplugin/typescript.lua
@@ -0,0 +1,6 @@
+-- Folding configurations for Neovim
+if vim.api.nvim_buf_line_count(0) >= 100 then
+  vim.o.foldmethod = "expr" -- Allow folding using the "nvim-treesitter" plugin
+  vim.o.foldexpr = "nvim_treesitter#foldexpr()" -- Configure the folding algorithm to be from the plugin
+  vim.o.foldlevel = 1 -- Configure the maximum indenting for the folding
+end
