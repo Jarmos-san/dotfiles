774c874bda818049cfa23c8158b186d77239f271
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-03-07 14:24:47 +0530

    chore:(neovim): update the colorscheme

--- dotfiles/.config/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,21 @@
 return {
-  {
-    "comfysage/evergarden",
-    priority = 1000,
-    opts = {
-      transparent_background = true,
-      contrast_dark = "medium",
+  "comfysage/evergarden",
+  priority = 1000,
+  opts = {
+    theme = {
+      variant = "fall",
+      accent = "green",
     },
-    config = true,
+    editor = {
+      transparent_background = false,
+      sign = { color = "none" },
+      float = {
+        color = "mantle",
+        invert_border = false,
+      },
+      completion = {
+        color = "surface0",
+      },
+    },
   },
 }
