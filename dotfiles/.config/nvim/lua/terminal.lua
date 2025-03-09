d6dc07f05305a4b77346e2de49f022f8e299411f
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-03-09 20:57:17 +0530

    chore(neovim): cleanup the code for opening a terminal buffer

--- dotfiles/.config/nvim/lua/terminal.lua
+++ dotfiles/.config/nvim/lua/terminal.lua
@@ -2,54 +2,37 @@
 
 local M = {}
 
-local create_term = function(width, height, row, col, opts)
-  local buf = vim.api.nvim_create_buf(false, true)
-
-  -- Conditionally apply terminal aesthetics depending on its type - either floating or vertical split
-  if not opts then
-    vim.api.nvim_open_win(buf, true, {
-      relative = "editor",
-      width = width,
-      height = height,
-      row = row,
-      col = col,
-      style = opts.style,
-      border = opts.border,
-    })
-  else
-    vim.api.nvim_open_win(buf, true, {
-      relative = "editor",
-      width = width,
-      height = height,
-      row = row,
-      col = col,
-    })
-  end
-
-  -- Start the terminal shell using the value of "$SHELL"
-  vim.fn.termopen(vim.o.shell)
-end
-
 M.setup = {
   float = function()
-    local width = math.floor(vim.o.columns * 0.8) -- 80% of editor width
-    local height = math.floor(vim.o.lines * 0.5) -- 50% of editor height
-    local row = (vim.o.lines - height) / 2 -- Center vertically
-    local col = (vim.o.columns - width) / 2 -- Center horizontally
+    local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
 
-    -- Create the terminal inside the floating window
-    create_term(width, height, row, col, { style = "minimal", border = "rounded" })
+    -- Various settings of the window
+    local width = math.floor(vim.o.columns * 0.8) -- 80% of the screen size
+    local height = math.floor(vim.o.lines * 0.5) -- 50% of the screen size
+    local row = (vim.o.lines - height) / 2
+    local col = (vim.o.columns - width) / 2
+    local title = string.format("Terminal (%s)", vim.o.shell)
+
+    -- Configure the settings of the window
+    local opts = {
+      relative = "editor",
+      row = row,
+      col = col,
+      width = width,
+      height = height,
+      border = "rounded",
+      title = title,
+      title_pos = "center",
+    }
+
+    -- Create a floating window to render the terminal into
+    vim.api.nvim_open_win(buf, true, opts)
+
+    -- Open a terminal session in the floating window
+    vim.fn.termopen(vim.o.shell)
   end,
 
   vertical = function()
-    -- local width = math.floor(vim.o.columns * 0.5) -- Set the width for the new split (50% of the current width)
-    -- local height = vim.api.nvim_win_get_height(0) -- Use the current window's height
-    -- local row = 0 -- Start from the top of the window
-    -- local col = vim.api.nvim_win_get_width(0) -- Place it at the right side of the current window
-
-    -- Create the terminal inside the vertical split
-    -- create_term(width, height, row, col, {})
-    -- TODO: Refactor the function
     local buf = vim.api.nvim_create_buf(false, true)
 
     -- Create a buffer to open the terminal in
