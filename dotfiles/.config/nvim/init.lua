a6183a01d6f6a4207f6fbf8c0b2984c4c099645d
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-06-15 11:30:21 +0530

    chore(neovim): bootstrap `lazy.nvim` package manager

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,6 +1,20 @@
 -- Path to install "lazy.nvim" at
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
 -- Update the Neovim runtimepath for "lazy.nvim" to source the plugins.
 vim.opt.rtp:prepend(lazypath)
 
