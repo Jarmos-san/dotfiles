674e4a37b3f126bd7b5e7e5cccbfa67a4d84a2a5
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-06-15 15:40:26 +0530

    chore(neovim): update the Treesitter plugin (#44)

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,13 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    cmd = { "TSInstall", "TSUpdate", "TSUpdateSync" },
-    event = { "BufNewFile", "BufRead" },
-    opts = {
-      -- Ensure the parsers for these languages are compulsarily installed
-      ensure_installed = {
+    lazy = false,
+    branch = "main",
+    build = function()
+      require("nvim-treesitter").update():wait(300000)
+    end,
+    config = function()
+      local parsers = {
         "astro",
         "bash",
         "comment",
@@ -44,84 +46,9 @@ return {
         "vim",
         "vimdoc",
         "vue",
-      },
-      highlight = { -- Enable syntax highlighting using the Treesitter parsers
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { -- Disable Treesitter-based indentation since its errorneous
-        enable = true,
-        disable = { "python" },
-      },
-      incremental_selection = { -- Incrementally select content powered by Treesitter
-        enable = true,
-      },
-      autotag = { -- Enable adding automatic HTML/JSX closing tags based on Treesitter queries
-        enable = true,
-      },
-      pairs = { -- Enable bracket pair highlighting
-        enable = true,
-        -- highlight_pair_events = { "CursorMoved" },
-        highlight_pair_events = { "CursorMoved" },
-      },
-      textobjects = { -- syntax-aware textobjects
-        select = {
-          enable = true,
-          keymaps = {
-            ["af"] = { query = "@function.outer", desc = "Select outer part of function" },
-            ["if"] = { query = "@function.inner", desc = "Select inner part of function" },
-            ["aC"] = { query = "@class.outer", desc = "Select outer part of class" },
-            ["iC"] = { query = "@class.inner", desc = "Select inner part of class" },
-            ["ac"] = { query = "@conditional.outer", desc = "Select outter part of if/else expression" },
-            ["ic"] = { query = "@conditional.inner", desc = "Select inner part of if/else expression" },
-            ["ab"] = { query = "@block.outer", desc = "Select outer part of a code block" },
-            ["ib"] = { query = "@block.inner", desc = "Select inner part of a code block" },
-            ["al"] = { query = "@loop.outer", desc = "Select around a loop statement" },
-            ["il"] = { query = "@loop.inner", desc = "Select inside a loop statement" },
-            -- TODO: Check out what they're supposed to be
-            -- ["is"] = { query = "@statement.inner", desc = "Select inside a statement" },
-            -- ["as"] ={ query = "@statement.outer", desc = "Select outside a statement" },
-            -- ["am"] = "@call.outer",
-            -- ["im"] = "@call.inner",
-            ["ad"] = { query = "@comment.outer", desc = "Select around a comment line" },
-            ["id"] = { query = "@comment.inner", desc = "Select inside a comment line" },
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = false,
-          goto_next_start = {
-            ["]f"] = { query = "@function.outer", desc = "Jump to the next function" },
-            ["]C"] = { query = "@class.outer", desc = "Jump to the next class" },
-          },
-          goto_previous_start = {
-            ["[f"] = { query = "@function.outer", desc = "Jump to previous function" },
-            ["[C"] = { query = "@class.outer", desc = "Jump to previous class" },
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>>"] = { query = "@parameter.inner", desc = "Swap inner parameters with successor" },
-            ["<leader>f>"] = { query = "@function.outer", desc = "Swap outer function with successor" },
-          },
-          swap_previous = {
-            ["<leader><"] = { query = "@parameter.inner", desc = "Swap inner parameters with predecessor" },
-            ["<leader>f<"] = { query = "@function.outer", desc = "Swap outer parameters with predecessor" },
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      -- Load the configurations table from above to initialise the plugin with
-      require("nvim-treesitter.configs").setup(opts)
-
-      -- Configure MDX files to load the Markdown Treesitter parser
-      vim.treesitter.language.register("markdown", "mdx")
+      }
+      require("nvim-treesitter").setup()
+      require("nvim-treesitter").install(parsers):wait(300000)
     end,
-    build = function()
-      vim.cmd("TSUpdateSync")
-    end,
-    dependencies = { "theHamsta/nvim-treesitter-pairs", "nvim-treesitter/nvim-treesitter-textobjects" },
   },
 }
