869668241fc0fe0dbc2c61e94c0fe6629e63ce7c
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-07-01 14:25:17 +0530

    chore(neovim): configure Neovim to recognise Ansible filetypes

--- dotfiles/.config/nvim/filetype.lua
+++ dotfiles/.config/nvim/filetype.lua
@@ -16,12 +16,15 @@ vim.filetype.add({
     ["env"] = "dotenv",
     ["tsconfig.json"] = "jsonc",
     ["cloudbeaver.conf"] = "jsonc",
+    ["playbook.yml"] = "yaml.ansible",
+    ["site.yml"] = "yaml.ansible",
   },
   -- Detect and apply filetypes based on certain patterns of the filenames
   pattern = {
     -- INFO: Match filenames like - ".env.example", ".env.local" and so on
     ["%.env%.[%w_.-]+"] = "dotenv",
     [".*/ansible/.*%.ya?ml"] = "yaml.ansible", -- Match all YAML files under the "ansible" directory
+    [".*/roles/.*%.ya?ml"] = "yaml.ansible",
     [".*/ansible/.*%.ya?ml%.j2"] = "yaml", -- Ansible template should be considered YAML files
     [".*/*.conf*"] = "conf", -- Config files (like "postgresql.conf" and so on)
     [".*/Caddyfile%.?.*"] = "caddy", -- Match files like "Caddyfile", "Caddyfile.j2", "Caddyfile.tmpl"
