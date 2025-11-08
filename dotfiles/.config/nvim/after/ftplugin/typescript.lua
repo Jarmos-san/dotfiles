a2ad39de288c3e97c9fa2fa797358e12b780184c
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-11-08 11:33:29 +0530

    chore(neovim): add diagnostic configs for TS

--- dotfiles/.config/nvim/after/ftplugin/typescript.lua
+++ dotfiles/.config/nvim/after/ftplugin/typescript.lua
@@ -7,3 +7,22 @@ if vim.api.nvim_buf_line_count(0) >= 100 then
   vim.o.foldexpr = "nvim_treesitter#foldexpr()" -- Configure the folding algorithm to be from the plugin
   vim.o.foldlevel = 1 -- Configure the maximum indenting for the folding
 end
+
+vim.diagnostic.config({ virtual_text = true })
+
+-- TODO: Move this autocommand to its own module/package
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = vim.api.nvim_get_current_buf(),
+  callback = function()
+    local hover_window_configs = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = "rounded",
+      source = "always",
+      prefix = " ",
+      scope = "cursor",
+    }
+
+    vim.diagnostic.open_float(nil, hover_window_configs)
+  end,
+})
