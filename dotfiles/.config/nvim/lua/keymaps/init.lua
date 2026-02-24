973c4dc9b27bbbb5e64be36b739499eee40e2b7e
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-02-24 16:25:40 +0530

    chore(nvim): add a module for lsp keymaps

--- dotfiles/.config/nvim/lua/keymaps/init.lua
+++ dotfiles/.config/nvim/lua/keymaps/init.lua
@@ -1,4 +1,11 @@
--- Module of keymaps & bindings which makes using Neovim a pleasure
+---@brief
+---
+---Entrypoint moule for registering all Neovim keymaps.
+---
+---This module serves as an orchestration layer that composes and initialises
+---all keymap namespaces in a deterministic order. Each required submodule is
+---expected to execute its keymap registration as a side effect of being
+---loaded.
 
 local M = {}
 
@@ -6,23 +13,7 @@ M.setup = function()
   require("keymaps.core")
   require("keymaps.terminal")
   require("keymaps.fzf")
-
-  local telescope = require("telescope.builtin")
-  local map = require("utils").map
-
-  -- Some keymaps for LSP capabilities
-  map("n", "gd", telescope.lsp_definitions, { desc = "Jump to the object definition" })
-  map("n", "gD", vim.lsp.buf.declaration, { desc = "Jump to the object declaration" })
-  map("n", "gT", telescope.lsp_type_definitions, { desc = "Get the type documentations" })
-  map("n", "gi", vim.lsp.buf.implementation, { desc = "Jump to the implementation" })
-  map("n", "K", vim.lsp.buf.hover, { desc = "Open the documentations of the object" })
-  map("n", "<C-S>", vim.lsp.buf.signature_help, { desc = "Get the help documentations" })
-  map("n", "gr", vim.lsp.buf.rename, { desc = "Rename the object under the cursor" })
-  map("n", "gR", telescope.lsp_references, { desc = "Jump to the reference of the object" })
-  map("n", "gra", vim.lsp.buf.code_action, { desc = "Open available code actions" })
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" })
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder" })
-  map("n", "<leader>wl", vim.lsp.buf.list_workspace_folders, { desc = "List workspace folders" })
+  require("keymaps.lsp")
 end
 
 return M
