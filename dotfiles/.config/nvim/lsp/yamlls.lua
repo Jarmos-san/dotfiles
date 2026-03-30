02b9fafd0a004d982c8936e60e95477939445914
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-03-30 18:57:11 +0530

    chore(nvim): add and configure LSP server for YAML

--- dotfiles/.config/nvim/lsp/yamlls.lua
+++ dotfiles/.config/nvim/lsp/yamlls.lua
@@ -1,12 +1,32 @@
+---@brief
+---
+--- https://github.com/redhat-developer/yaml-language-server
+---
+--- The LSP server can be installed via:
+--- ```sh
+--- pnpm add --global yaml-language-server
+--- ```
+
+---@type vim.lsp.Config
 return {
   cmd = { "yaml-language-server", "--stdio" },
-  filetypes = { "yaml" },
+  filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
   root_markers = { ".git" },
   settings = {
     redhat = {
-      telemetry = false,
+      telemetry = {
+        enable = false,
+      },
+    },
+    yaml = {
+      format = {
+        enable = true,
+      },
     },
     schemas = require("schemastore").yaml.schemas(),
     validate = true,
   },
+  on_init = function(client)
+    client.server_capabilities.documentFormattingProvider = true
+  end,
 }
