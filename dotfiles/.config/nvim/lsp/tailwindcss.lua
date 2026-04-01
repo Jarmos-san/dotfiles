a4749bf2f6907bb137a13360f12efe05caa2263d
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-04-01 18:02:32 +0530

    chore(nvim): setup the LSP server for Neovim

---
+++ dotfiles/.config/nvim/lsp/tailwindcss.lua
@@ -0,0 +1,33 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "tailwindcss-language-server", "--stdio" },
+  filetypes = { "vue" },
+  capabilities = {
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
+    },
+  },
+  settings = {
+    tailwindCSS = {
+      validate = true,
+      lint = {
+        cssConflict = "warning",
+        invalidApply = "error",
+        invalidScreen = "error",
+        invalidVariant = "error",
+        invalidConfigPath = "error",
+        invalidTailwindDirective = "error",
+        recommendedVariantOrder = "warning",
+      },
+      classAttributes = {
+        "class",
+        "className",
+        "class:list",
+        "classList",
+        "ngClass",
+      },
+    },
+  },
+}
