2b3dc6a459081abd53d4a25e2b857c8e3c8fd905
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-04-27 22:04:33 +0530

    chore(nvim): setup some Go specific configurations

--- dotfiles/.config/nvim/after/ftplugin/go.lua
+++ dotfiles/.config/nvim/after/ftplugin/go.lua
@@ -2,7 +2,6 @@
 
 local autocmd = require("utils").autocmd
 local augroup = require("utils").augroup
-local map = require("utils").map
 
 autocmd("BufWritePost", {
   desc = "Format the buffer contents after save",
@@ -12,19 +11,22 @@ autocmd("BufWritePost", {
   end,
 })
 
-map("n", "<F5>", "<CMD>terminal go run %<CR>")
+-- Setup a keymap to invoke the Go executable
+if vim.fn.expand("%:t") == "main.go" then
+  vim.keymap.set("n", "<F5>", "<CMD>terminal go run %<CR>")
+end
+
+-- Enable the Treesitter parsing
+vim.treesitter.start()
 
 -- Configure the indent-based folds for Go buffers
 if vim.api.nvim_buf_line_count(0) >= 100 then
-  vim.opt.foldmethod = "expr"
-  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-  vim.opt.foldlevel = 0 -- Start with folds closed
-  vim.opt.foldcolumn = "auto" -- Show fold indicators on the left margin
-  vim.opt.foldnestmax = 99 -- Maximum depth of folds
+  vim.wo.foldmethod = "expr"
+  vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  vim.wo.foldlevel = 0 -- Start with folds closed
+  vim.wo.foldcolumn = "auto" -- Show fold indicators on the left margin
+  vim.wo.foldnestmax = 99 -- Maximum depth of folds
 end
-
--- Start the Treesitter parsing process
-vim.treesitter.start()
 
 -- Start the "gopls" LSP server
 vim.lsp.enable("gopls", true)
