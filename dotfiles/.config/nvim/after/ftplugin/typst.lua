959d499014809a89b10f798796a48a7889ff026b
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-05-04 17:42:52 +0530

    chore(nvim): add some line wraps for Typst code

--- dotfiles/.config/nvim/after/ftplugin/typst.lua
+++ dotfiles/.config/nvim/after/ftplugin/typst.lua
@@ -3,3 +3,7 @@ vim.treesitter.start()
 
 -- Enable the "tinymist" LSP server for Typst files
 vim.lsp.enable("tinymist", true)
+
+-- Setup linebreaks and soft wraps for easier viewing of the contents in a buffer
+vim.opt.wrap = true
+vim.opt.linebreak = true
